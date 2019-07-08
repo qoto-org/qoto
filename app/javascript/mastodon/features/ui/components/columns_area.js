8ffcf80282d5619db57862a58c188e5c0c3e02e3
noellabo <noel.yoshiba@gmail.com>
2019-07-08 13:03:08 +0900

    Show navigation panel in multi-column WebUI

--- app/javascript/mastodon/features/ui/components/columns_area.js
+++ app/javascript/mastodon/features/ui/components/columns_area.js
@@ -229,6 +229,11 @@ class ColumnsArea extends ImmutablePureComponent {
         })}
 
         {React.Children.map(children, child => React.cloneElement(child, { multiColumn: true }))}
+        <div className='columns-area__panels__pane columns-area__panels__pane--start columns-area__panels__pane--navigational'>
+          <div className='columns-area__panels__pane__inner'>
+            <NavigationPanel />
+          </div>
+        </div>
       </div>
     );
   }
