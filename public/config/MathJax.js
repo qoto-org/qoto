0d43931b8d9419acfe94458e1bf5279ad476c687
Jeffrey Phillips Freeman <jeffrey.freeman@syncleus.com>
2019-08-29 02:45:35 +0200

    Foxed typo in MathJax config.

--- public/config/MathJax.js
+++ public/config/MathJax.js
@@ -2,7 +2,7 @@ MathJax.Hub.Config({
     extensions: ["tex2jax.js","TeX/AMSmath.js","TeX/AMSsymbols.js", "TeX/noErrors.js"],
     jax: ["input/TeX","output/HTML-CSS"],
     tex2jax: {
-        inlineMath: [[["\\(","\\)"]],
+        inlineMath: [["\\(","\\)"]],
         displayMath: [ ["\\[","\\]"] ],
         processEscapes: true,
     },
