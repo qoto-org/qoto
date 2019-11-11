b8db62ad717745b0dc573f41b1307e0400c11a74
noellabo <noel.yoshiba@gmail.com>
2019-11-12 06:21:32 +0900

    Allow data: in font-src of content security policy

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -24,7 +24,7 @@ Rails.application.config.content_security_policy do |p|
   p.base_uri        :none
   p.default_src     :none
   p.frame_ancestors :none
-  p.font_src        :self, assets_host, bootstrap_cdn_host
+  p.font_src        :self, :data, assets_host, bootstrap_cdn_host
   p.img_src         :self, :https, :data, :blob, assets_host, instance_ticker_img_host
   p.style_src       :self, :unsafe_inline, assets_host, instance_ticker_host, bootstrap_cdn_host
   p.media_src       :self, :https, :data, assets_host
