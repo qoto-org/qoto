db313fd1ec647a248d547daee0c7c8f3e908c862
noellabo <noel.yoshiba@gmail.com>
2019-11-12 20:20:08 +0900

    Compatible with Misskey quote

--- app/lib/activitypub/activity/create.rb
+++ app/lib/activitypub/activity/create.rb
@@ -117,7 +117,7 @@ class ActivityPub::Activity::Create < ActivityPub::Activity
         conversation: conversation_from_uri(@object['conversation']),
         media_attachment_ids: process_attachments.take(4).map(&:id),
         poll: process_poll,
-        quote: quote_from_url(@object['quoteUrl']),
+        quote: quote,
       }
     end
   end
@@ -388,7 +388,9 @@ class ActivityPub::Activity::Create < ActivityPub::Activity
   def text_from_content
     return Formatter.instance.linkify([[text_from_name, text_from_summary.presence].compact.join("\n\n"), object_url || object_uri].join(' ')) if converted_object_type?
 
-    if @object['content'].present?
+    if @object['quoteUrl'].blank? && @object['_misskey_quote'].present?
+      Formatter.instance.linkify(@object['_misskey_content'])
+    elsif @object['content'].present?
       @object['content']
     elsif content_language_map?
       @object['contentMap'].values.first
@@ -519,10 +521,16 @@ class ActivityPub::Activity::Create < ActivityPub::Activity
     { redis: Redis.current, key: "vote:#{replied_to_status.poll_id}:#{@account.id}" }
   end
 
+  def quote
+    @quote ||= quote_from_url(@object['quoteUrl'] || @object['_misskey_quote'])
+  end
+
   def quote_from_url(url)
     return nil if url.nil?
 
     quote = ResolveURLService.new.call(url)
     status_from_uri(quote.uri) if quote
+  rescue
+    nil
   end
 end
