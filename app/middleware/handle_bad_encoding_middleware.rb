95ecd3e50f550515f1ca0a311c16c1af997ea863
Eugen Rochko <eugen@zeonfederated.com>
2020-01-02 23:10:42 +0100

    Fix more uncaught query param encoding errors

--- app/middleware/handle_bad_encoding_middleware.rb
+++ app/middleware/handle_bad_encoding_middleware.rb
@@ -2,15 +2,34 @@
 # See: https://jamescrisp.org/2018/05/28/fixing-invalid-query-parameters-invalid-encoding-in-a-rails-app/
 
 class HandleBadEncodingMiddleware
+  SANITIZE_ENV_KEYS = %w(
+    HTTP_REFERER
+    PATH_INFO
+    REQUEST_URI
+    REQUEST_PATH
+    QUERY_STRING
+  ).freeze
+
   def initialize(app)
     @app = app
   end
 
   def call(env)
+    SANITIZE_ENV_KEYS.each do |key|
+      str = URI.decode(env[key].to_s).force_encoding('UTF-8')
+
+      return [400, {}, ['Bad request']] unless str.valid_encoding?
+    end
+
     begin
-      Rack::Utils.parse_nested_query(env['QUERY_STRING'].to_s)
-    rescue Rack::Utils::InvalidParameterError
-      env['QUERY_STRING'] = ''
+      request = Rack::Request.new(env.dup)
+      request.params
+    rescue ArgumentError => e
+      if e.message =~ /invalid %-encoding/
+        return [400, {}, ['Bad request']]
+      else
+        raise e
+      end
     end
 
     @app.call(env)
