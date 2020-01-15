3bacd13cf31b51674b700e0edee2b77ee9549da5
noellabo <noel.yoshiba@gmail.com>
2020-01-16 07:47:17 +0900

    Add list to tab bar

--- app/javascript/mastodon/features/compose/index.js
+++ app/javascript/mastodon/features/compose/index.js
@@ -107,6 +107,7 @@ class Compose extends React.PureComponent {
           {!columns.some(column => column.get('id') === 'PUBLIC') && (
             <Link to='/timelines/public' className='drawer__tab' title={intl.formatMessage(messages.public)} aria-label={intl.formatMessage(messages.public)}><Icon id='globe' fixedWidth /></Link>
           )}
+          <Link to='/lists' className='drawer__tab' title={intl.formatMessage(messages.lists)} aria-label={intl.formatMessage(messages.lists)}><Icon id='list-ul' fixedWidth /></Link>
           <a href='/settings/preferences' className='drawer__tab' title={intl.formatMessage(messages.preferences)} aria-label={intl.formatMessage(messages.preferences)}><Icon id='cog' fixedWidth /></a>
           <a href='/auth/sign_out' className='drawer__tab' title={intl.formatMessage(messages.logout)} aria-label={intl.formatMessage(messages.logout)} onClick={this.handleLogoutClick}><Icon id='sign-out' fixedWidth /></a>
         </nav>
