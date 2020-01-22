02a1f7e18ea59d9444e48bbe7a2a6415f30a9892
Jeffrey Phillips Freeman <jeffrey.freeman@syncleus.com>
2020-01-22 15:35:15 +0100

    Revert: "Set up elasticsearch 7 to use sudachi"
    This reverts commit ceabee16849bed312d6955d2a26f07e29c852e17.

--- app/chewy/statuses_index.rb
+++ app/chewy/statuses_index.rb
@@ -2,26 +2,31 @@
 
 class StatusesIndex < Chewy::Index
   settings index: { refresh_interval: '15m' }, analysis: {
-    tokenizer: {
-      sudachi_tokenizer: {
-        type: 'sudachi_tokenizer',
-        mode: 'search',
-        discard_punctuation: true,
-        resources_path: '/etc/elasticsearch',
-        settings_path: '/etc/elasticsearch/sudachi.json',
+    filter: {
+      english_stop: {
+        type: 'stop',
+        stopwords: '_english_',
+      },
+      english_stemmer: {
+        type: 'stemmer',
+        language: 'english',
+      },
+      english_possessive_stemmer: {
+        type: 'stemmer',
+        language: 'possessive_english',
       },
     },
     analyzer: {
       content: {
+        tokenizer: 'uax_url_email',
         filter: %w(
+          english_possessive_stemmer
           lowercase
+          asciifolding
           cjk_width
-          sudachi_part_of_speech
-          sudachi_ja_stop
-          sudachi_baseform
+          english_stop
+          english_stemmer
         ),
-        tokenizer: 'sudachi_tokenizer',
-        type: 'custom',
       },
     },
   }
