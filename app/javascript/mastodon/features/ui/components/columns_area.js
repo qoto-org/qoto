c9e3ac4309a845e928bf8bca86feadeff1a3f1e4
Jeffrey Phillips Freeman <jeffrey.freeman@syncleus.com>
2020-01-22 21:09:37 +0100

    Fix: Restored navigation panel in simple ui.

--- app/javascript/mastodon/features/ui/components/columns_area.js
+++ app/javascript/mastodon/features/ui/components/columns_area.js
@@ -208,6 +208,7 @@ class ColumnsArea extends ImmutablePureComponent {
 
           <div className='columns-area__panels__pane columns-area__panels__pane--start columns-area__panels__pane--navigational'>
             <div className='columns-area__panels__pane__inner'>
+              <NavigationPanel />
             </div>
           </div>
 
