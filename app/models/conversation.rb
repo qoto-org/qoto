0bad2f7cc9aa1a256ab13b154c77aea6bc50477c
Eugen Rochko <eugen@zeonfederated.com>
2020-09-03 00:58:11 +0200

    WIP

--- app/models/conversation.rb
+++ app/models/conversation.rb
@@ -3,10 +3,13 @@
 #
 # Table name: conversations
 #
-#  id         :bigint(8)        not null, primary key
-#  uri        :string
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
+#  id                :bigint(8)        not null, primary key
+#  uri               :string
+#  created_at        :datetime         not null
+#  updated_at        :datetime         not null
+#  parent_status_id  :bigint(8)
+#  parent_account_id :bigint(8)
+#  inbox_url         :string
 #
 
 class Conversation < ApplicationRecord
