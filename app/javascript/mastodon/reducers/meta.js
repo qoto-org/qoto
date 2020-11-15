a1e06d0ac6ca901258f2cfab63ce18016931a588
Eugen Rochko <eugen@zeonfederated.com>
2020-11-15 02:53:10 +0100

    Fix pop-out player appearing on mobile screens in web UI
    Fix #15092

--- app/javascript/mastodon/reducers/meta.js
+++ app/javascript/mastodon/reducers/meta.js
@@ -1,15 +1,21 @@
-import { STORE_HYDRATE } from '../actions/store';
+import { STORE_HYDRATE } from 'mastodon/actions/store';
+import { APP_LAYOUT_CHANGE } from 'mastodon/actions/app';
 import { Map as ImmutableMap } from 'immutable';
+import { isMobile } from 'mastodon/is_mobile';
+import { forceSingleColumn } from 'mastodon/initial_state';
 
 const initialState = ImmutableMap({
   streaming_api_base_url: null,
   access_token: null,
+  layout: isMobile(window.innerWidth) ? 'mobile' : (forceSingleColumn ? 'single-column' : 'multi-column'),
 });
 
 export default function meta(state = initialState, action) {
   switch(action.type) {
   case STORE_HYDRATE:
     return state.merge(action.state.get('meta'));
+  case APP_LAYOUT_CHANGE:
+    return state.set('layout', action.layout);
   default:
     return state;
   }
