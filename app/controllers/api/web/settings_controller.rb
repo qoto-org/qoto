0ebfd53972a2d8aa4e9c92289927b493a4e94ebd
Eugen Rochko <eugen@zeonfederated.com>
2021-02-12 05:06:28 +0100

    Refactor Api::Web::SettingsController

--- app/controllers/api/web/settings_controller.rb
+++ app/controllers/api/web/settings_controller.rb
@@ -2,17 +2,20 @@
 
 class Api::Web::SettingsController < Api::Web::BaseController
   before_action :require_user!
+  before_action :set_setting
 
   def update
-    setting.data = params[:data]
-    setting.save!
-
+    @setting.update!(resource_params)
     render_empty
   end
 
   private
 
-  def setting
-    @_setting ||= ::Web::Setting.where(user: current_user).first_or_initialize(user: current_user)
+  def set_setting
+    @setting = ::Web::Setting.where(user: current_user).first_or_initialize(user: current_user)
+  end
+
+  def resource_params
+    params.permit(:data)
   end
 end
