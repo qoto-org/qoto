0e5467f0b46afc86661c02c9b7ea260b52bffbff
Eugen Rochko <eugen@zeonfederated.com>
2021-04-03 02:21:06 +0200

    Add system checks to dashboard in admin UI

---
+++ app/lib/admin/system_check.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+class Admin::SystemCheck
+  ACTIVE_CHECKS = [
+    Admin::SystemCheck::SidekiqProcessCheck,
+    Admin::SystemCheck::RulesCheck,
+  ].freeze
+
+  def self.perform
+    ACTIVE_CHECKS.each_with_object([]) do |klass, arr|
+      check = klass.new
+
+      if check.pass?
+        arr
+      else
+        arr << check.message
+      end
+    end
+  end
+end
