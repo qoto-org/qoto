9ae69f8a548ad146f806e5437ba246e1a737b325
Eugen Rochko <eugen@zeonfederated.com>
2022-04-26 18:16:52 +0200

    Add a connection pool for Redis

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,14 +1,15 @@
 # frozen_string_literal: true
 
-redis_connection = Redis.new(
-  url: ENV['REDIS_URL'],
-  driver: :hiredis
-)
-
+url       = ENV['REDIS_URL']
 namespace = ENV.fetch('REDIS_NAMESPACE') { nil }
+pool_size = ENV['REDIS_POOL'] || ENV['MAX_THREADS'] || 5
 
-if namespace
-  Redis.current = Redis::Namespace.new(namespace, redis: redis_connection)
-else
-  Redis.current = redis_connection
+Redis.current = ConnectionPool::Wrapper.new(size: pool_size, timeout: 3) do
+  connection = Redis.new(url: url, driver: :hiredis)
+
+  if namespace
+    Redis::Namespace.new(namespace, redis: connection)
+  else
+    connection
+  end
 end
